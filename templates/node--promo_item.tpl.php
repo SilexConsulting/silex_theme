89b79181875522f4c331af0184a24fdd13c72bbf
Noel Sharpe <noels@radnetwork.co.uk>
2015-01-27 21:09:27 +0000

    Typo.

--- templates/node--promo_item.tpl.php
+++ templates/node--promo_item.tpl.php
@@ -2,5 +2,5 @@
             <a href="<?php print url($field_target[$language][0]["value"]); ?>"><img src="<?php print file_create_url($field_image[$language][0]['uri']);?>"></a>
             <h2><a href="<?php print url($field_target[$language][0]["value"]); ?>"><?php print $title ?></a></h2>
             <p><a href="<?php print url($field_target[$language][0]["value"]); ?>"><?php print $body[0]["summary"] ?></a></p>
-            <p><a class="btn" href="<?php print url($field_target[$language][0]["value"]); ?>"><?php print check_plain($field_target_text[$language][0]["value"];) ?></a></p>
+            <p><a class="btn" href="<?php print url($field_target[$language][0]["value"]); ?>"><?php print check_plain($field_target_text[$language][0]["value"]) ?></a></p>
         </div><!-- /.span4 -->
